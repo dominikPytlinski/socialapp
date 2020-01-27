390c2b83e30f73a7f4168d1fbefb88047e90f993
Dominik <dominik_p@wp.pl>
2020-01-27 20:12:15 +0100

    added updatePost and getUserPosts

--- middleware/post.js
+++ middleware/post.js
@@ -1,34 +1,71 @@
 //Models
 const postModel = require('../models/Post');
-const userModel = require('../models/User');
 
+//Helpers
+const { setErrors } = require('../helpers/setErrors');
+ 
 exports.getAllPosts = async (req, res, next) => {
-    const posts = [];
-    const allPosts = await postModel.find({}).populate('creator');
-    allPosts.map((post) => {
-        posts.push({
-            ...post._doc,
-            creator: {
-                _id: post._doc.creator._id,
-                email: post._doc.creator.email,
-                nickName: post._doc.creator.nickName
-            }
+    try {
+        const posts = [];
+        const allPosts = await postModel.find({}).populate('creator');
+        allPosts.map((post) => {
+            posts.push({
+                ...post._doc,
+                creator: {
+                    _id: post._doc.creator._id,
+                    email: post._doc.creator.email,
+                    nickName: post._doc.creator.nickName
+                }
+            })
         })
-    })
-    return res.status(200).json({
-        level: 'Success',
-        message: 'Posts found',
-        data: posts
-    });
+        return res.status(200).json({
+            data: posts
+        });
+    } catch (error) {
+        setErrors(error, res);
+    }
+}
+
+exports.getUserPosts = async (req, res, next) => {
+    try {
+        const requestedUserId = (req.role == 'admin' && req.params.id) ? req.params.id : req.userId;
+        const posts = [];
+        const userPosts = await postModel.find({
+            creator: requestedUserId
+        }).populate('creator');
+        if(userPosts.length > 0) {
+            userPosts.map(post => {
+                posts.push({
+                    ...post._doc,
+                    creator: {
+                        _id: post._doc.creator._id,
+                        email: post._doc.creator.email,
+                        nickName: post._doc.creator.nickName
+                    }
+                })
+            });
+            return res.status(200).json({
+                data: posts
+            });
+        } else {
+            return res.status(404).json({
+                data: 'No results'
+            });
+        }
+    } catch (error) {
+        setErrors(error, res);
+    }
 }
 
 exports.addPost = async (req, res, next) => {
     try {
         const { title, body } = req.body;
-        if(!title || !body ) return res.status(400).json({
-            level: 'Error',
-            message: 'All fields are required'
-        });
+        if(!title || !body ) {
+            const e = new Error('All fields are required');
+            e.name = 'CustomError',
+            e.code = 400;
+            throw e;
+        }
         const newPost = new postModel({
             title: title,
             body: body,
@@ -37,7 +74,6 @@ exports.addPost = async (req, res, next) => {
         let result = await newPost.save();
         result = await result.populate('creator').execPopulate();
         return res.status(201).json({
-            level: 'Success',
             message: 'Post created successfully',
             data: {
                 ...result._doc,
@@ -49,23 +85,74 @@ exports.addPost = async (req, res, next) => {
             }
         });
     } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
+        setErrors(error, res);
     }
 }
 
 exports.deletePost = async (req, res, next) => {
     try {
-        if(req.role == 'admin') await postModel.findByIdAndDelete(req.params.id)
-        else await postModel.findOneAndDelete({
+        let deletedPost = null;
+        if(req.role == 'admin') deletedPost = await postModel.findByIdAndDelete(req.params.id)
+        else deletedPost = await postModel.findOneAndDelete({
             _id: req.params.id,
             creator: req.userId
         });
-        return res.status(200).json({
-            level: 'Success',
-            message: 'Post delted successfully'
-        });
+        if(deletedPost) {
+            return res.status(200).json({
+                level: 'Success',
+                message: 'Post delted successfully'
+            });
+        } else {
+            const e = new Error('Something went wrong');
+            e.name = 'CustomError',
+            e.code = 500;
+            throw e;
+        }
     } catch (error) {
-        return res.status(500).json(error);
+        setErrors(error, res);
+    }
+}
+
+exports.updatePost = async (req, res, next) => {
+    try {
+        const { title, body } = req.body;
+        if(!title || !body) {
+            const e = new Error('All fields are required');
+            e.name = 'CustomError',
+            e.code = 400;
+            throw e;
+        }
+        const post = {
+            title,
+            body
+        }
+        let updatedPost = null;
+        if(req.role == 'admin') updatedPost = await postModel.findByIdAndUpdate(req.params.id, post, { new: true }).populate('creator');
+        else updatedPost = await postModel.findOneAndUpdate({
+            _id: req.params.id,
+            creator: req.userId
+        }, {
+            new: true
+        }).populate('creator');
+        if(updatedPost) {
+            res.status(200).json({
+                message: 'Post updated successfully',
+                data: {
+                    ...updatedPost._doc,
+                    creator: {
+                        _id: updatedPost._doc.creator._id,
+                        email: updatedPost._doc.creator.email,
+                        nickName: updatedPost._doc.creator.nickName
+                    }
+                }
+            })
+        } else {
+            const e = new Error('Something went wrong');
+            e.name = 'CustomError';
+            e.code = 500;
+            throw e;
+        }
+    } catch (error) {
+        setErrors(error, res); 
     }
 }
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 //Middleware
-const { getAllPosts, addPost, deletePost } = require('../middleware/post');
+const { getAllPosts, addPost, deletePost, updatePost, getUserPosts } = require('../middleware/post');
 const { isLogged } = require('../middleware/auth');
 
 router.get('/', getAllPosts);
+router.get('/user/:id?', isLogged, getUserPosts);
 router.post('/', isLogged, addPost);
 router.delete('/:id', isLogged, deletePost);
+router.put('/:id', isLogged, updatePost);
 
 module.exports = router;
