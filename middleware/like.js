86b7c8855bdb715d91dd246954907dc9a3989618
Dominik <dominik_p@wp.pl>
2020-01-28 18:58:32 +0100

    added deleteLike

--- middleware/like.js
+++ middleware/like.js
@@ -11,7 +11,7 @@ exports.addLike = async (req, res, next) => {
     });
     let createdLike = await newLike.save();
     createdLike = await createdLike.populate('user').execPopulate();
-    if(createdLike) res.status(201).json({
+    if(createdLike) return res.status(201).json({
         message: 'Liked',
         data: {
             ...createdLike._doc,
@@ -23,6 +23,21 @@ exports.addLike = async (req, res, next) => {
                 updatedAt: createdLike._doc.user.updatedAt
             }
         }
+    });
+    else setErrors(500, 'Something went wrong');
+}
+
+exports.deleteLike = async (req, res, next) => {
+    const like = await likeModel.findById(req.params.id);
+    console.log(like)
+    console.log(req.userId)
+    if(!like) return res.status(400).json({
+        data: 'No results'
+    })
+    if(like.user != req.userId && req.role != 'admin') setErrors(401, 'Unauthorized');
+    const deletedLike = await likeModel.findByIdAndDelete(req.params.id);
+    if(deletedLike) return res.status(200).json({
+        message: 'Unliked'
     });
     else setErrors(500, 'Something went wrong');
 }
--- routes/likesRoutes.js
+++ routes/likesRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 //Middleware
-const { addLike } = require('../middleware/like');
+const { addLike, deleteLike } = require('../middleware/like');
 const { isLogged } = require('../middleware/auth');
  
 router.post('/', isLogged, addLike);
+router.delete('/:id', isLogged, deleteLike);
 
 module.exports = router;
