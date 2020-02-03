d388f96bbaeb237148048f7165b4eeaac1407630
unknown <dominik_p@wp.pl>
2020-02-03 19:28:59 +0100

    added file upload

--- server.js
+++ server.js
@@ -2,12 +2,33 @@ const express = require('express');
 const mongosse = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const multer = require('multer');
 
 require('dotenv').config();
+
+const { isLogged } = require('./middleware/auth');
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+let filename;
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        filename = Date.now() + '-' + file.originalname;
+        cb(null, filename);
+    }
+});
+const upload = multer({ 
+    storage: storage,
+    fileFilter: function(req, file, cb) {
+        if(file.mimetype != 'image/png' && file.mimetype != 'image/jpeg') cb(new Error('incorret type'), false);
+        else cb(null, true);
+    }
+ });
 
 //Rutes
 const usersRouts = require('./routes/usersRoutes');
@@ -22,6 +43,13 @@ app.use('/users', usersRouts);
 app.use('/roles', rolesRoutes);
 app.use('/login', loginRoutes);
 app.use('/posts', postsRoutes);
+app.use('/upload', isLogged, upload.single('img'), (req, res, next) => {
+    return res.status(200).json({
+        path: `http://localhost:4000/uploads/${filename}`
+    });
+});
+
+app.use('/uploads', express.static('uploads'));
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-kszkn.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 const mongoOption = {
