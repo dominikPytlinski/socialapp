1f1c50cdfe856cc191212ec52e4210e38c9844a3
unknown <dominik_p@wp.pl>
2020-02-08 11:47:51 +0100

    new beginig

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,11 @@
-import React, { Component, Fragment } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Pages/Home';
-import Login from './Pages/Login';
-import Singup from './Pages/Signup';
-import Header from './Components/Header';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
-  render()
-  {
-    return (
-      <BrowserRouter>
-        <Fragment>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Singup} />
-          </Switch>
-        </Fragment>
-      </BrowserRouter>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
 export default App;
--- client/src/Components/Loading.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Loading = () => <div className="loader"></div>
-
-export default Loading;
