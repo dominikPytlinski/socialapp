e2b71a8f2bf088acffa435dd165b5161ba971467
unknown <dominik_p@wp.pl>
2020-02-08 17:07:24 +0100

    added login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleChangeEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handleChangePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const loginData = {
+            email: email,
+            password: password
+        }
+        axios.post('http://localhost:4000/login', loginData)
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(err => console.log(err));
+    }
+
     return (
-        <div>
-            
-        </div>
+        <main className="container">
+            <div className="login-form">
+                <form className="form" onSubmit={handleSubmit}>
+                    <div className="form-control">
+                        <label htmlFor="email">
+                            Email:
+                        </label>
+                        <input type="email" name="email" id="email" value={email} onChange={handleChangeEmail} />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">
+                            Hasło:
+                        </label>
+                        <input type="password" name="password" id="password" value={password} onChange={handleChangePassword} />
+                    </div>
+                    <div className="form-control">
+                        <button className="btn btn-primary" type="submit">
+                            Zaloguj
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </main>
     )
 }
 
