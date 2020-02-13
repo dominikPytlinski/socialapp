8946033d88bb90af68c6fb4074682b0d9cde67a8
Dominik <dominik_p@wp.pl>
2020-02-13 20:13:39 +0100

    changed PostListItem to using MUI

--- client/src/components/PostsListItem.js
+++ client/src/components/PostsListItem.js
@@ -2,20 +2,60 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import relativTime from 'dayjs/plugin/relativeTime';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    postListItem: {
+        marginBottom: '.5rem',
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    media: {
+        width: 150,
+        height: 150
+    },
+    postDetails: {
+        display: 'flex',
+        flexDirection: 'column'
+    }
+})
 
 const PostsListItem = ({ post: { likes, comments, _id, title, body, createdAt, updatedAt, creator }}) => {
     dayjs.extend(relativTime);
+    const classes = useStyles();
     return (
-        <div className="posts-list-item">
-            <div className="img-wrapper">
-                <img src={creator.image} alt="user" />
-            </div>
-            <div className="post-details">
-                <h3><Link to="#" >{creator.nickName}</Link></h3>
-                <span className="date">{dayjs(createdAt).fromNow()}</span>
-                <p>{body}</p>
-            </div>
-        </div>
+        <Card className={classes.postListItem}>
+            <CardMedia
+                className={classes.media}
+                image={creator.image}
+                alt="user"
+            />
+            <CardContent className={classes.postDetails}>
+                <Typography
+                    variant="h4"
+                    component={Link}
+                    to="#"
+                    color="primary"
+                >
+                    {creator.nickName}
+                </Typography>
+                <Typography
+                    variant="caption"
+                >
+                    {dayjs(createdAt).fromNow()}
+                </Typography>
+                <Typography
+                    variant="body2"
+                >
+                    {body}
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
 
