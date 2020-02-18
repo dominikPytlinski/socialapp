2580eb4240b201298608e3141eae6f770a0b8b1b
Dominik <dominik_p@wp.pl>
2020-02-18 12:39:54 +0100

    dismised warnings in PostsListItem

--- client/src/components/PostsListItem.js
+++ client/src/components/PostsListItem.js
@@ -1,11 +1,9 @@
 import React, { Fragment } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import relativTime from 'dayjs/plugin/relativeTime';
-import axios from 'axios';
 //Redux
-import { LIKE_POST, UNLIKE_POST } from '../redux/types';
+import { useSelector } from 'react-redux';
 //MUI
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -47,43 +45,6 @@ const PostsListItem = ({ post }) => {
     dayjs.extend(relativTime);
     const user = useSelector(state => state.user);
     const classes = useStyles();
-    const dispatch = useDispatch();
-
-    const handleLike = () => {
-        const likeData = {
-            post: post._id
-        }
-        axios.post('http://localhost:4000/posts/like', likeData)
-            .then(() => {
-                const data = {
-                    postId: post._id,
-                    userId: user.id
-                }
-                dispatch({
-                    type: LIKE_POST,
-                    payload: data
-                })
-            })
-            .catch();
-    }
-
-    const handleUnlike = () => {
-        const unlikeData = {
-            post: post._id
-        }
-        axios.post('http://localhost:4000/posts/unlike', unlikeData)
-            .then(() => {
-                const data = {
-                    postId: post._id,
-                    userId: user.id
-                }
-                dispatch({
-                    type: UNLIKE_POST,
-                    payload: data
-                })
-            })
-            .catch();
-    }
 
     return (
         <Card className={classes.postListItem}>
